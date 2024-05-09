76a02b158fef4f3f68cc32702859588cfac928de
unknown <vincent.pradipta@binus.ac.id>
2024-05-10 02:54:08 +0700

    ADD: Github Authentication Implement

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,65 @@
 // src/index.ts
 import express from 'express';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
 const port = 8000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, TypeScript with Express!');
+// const fetch = async (...args: Parameters<typeof import('node-fetch')['default']>) => {
+//     const { default: fetch } = await import('node-fetch');
+//     return fetch(...args);
+// };
+
+const CLIENT_ID = process.env.CLIENT_ID || "";
+const CLIENT_SECRET = process.env.CLIENT_SECRET || "";
+const REPO_PERSONAL_TOKEN = process.env.REPO_PERSONAL_TOKEN || "";
+
+app.get('/api/v1/', (req, res) => {
+    return res.send('QuanC API Ready!');
+});
+
+app.get('/getAccessToken', async (req, res) => {
+    const param = '?client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET + '&code=' + req.query.code;
+    console.log(param)
+    await fetch('http://github.com/login/oauth/access_token' + param, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+        .then((response) => { return response.json() })
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+});
+
+app.get('/getUserData', async (req, res) => {
+    const auth = req.get('Authorization');
+    if (auth === undefined) {
+        return res.status(401).send('Unauthorized');
+    }
+    await fetch('https://api.github.com/user', {
+        method: 'GET',
+        headers: {
+            'Authorization': auth,
+        }
+    })
+        .then((response) => {
+            console.log(response);
+            return response.json();
+        })
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
