0287c13ac2cfe0164b6f7ec0996116dbc45d4a2e
unknown <vincent.pradipta@binus.ac.id>
2024-05-16 23:23:11 +0700

    FIX: Add async for await function

--- populate_random_challenges.js
+++ populate_random_challenges.js
@@ -12,15 +12,20 @@ function getRandomPoints(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-for (var i = 0; i < 113; i++) {
-    var title = getRandomTitle(10);
-    points = getRandomPoints(10, 25);
-    data = {
-        challenge_title: title,
-        repo_link: `https://github.com/xhfmvls/${title}`,
-        points: points,
-        total_test_case: 17
-    }
+const main = async () => {
+    for (var i = 0; i < 113; i++) {
+        var title = getRandomTitle(10);
+        points = getRandomPoints(10, 25);
+        data = {
+            challenge_title: title,
+            repo_link: `https://github.com/xhfmvls/${title}`,
+            points: points,
+            total_test_case: 17
+        }
 
-    await prisma.challenge.create(data);
+        // await prisma.challenge.create(data);
+        console.log(data)
+    }
 }
+
+main()
