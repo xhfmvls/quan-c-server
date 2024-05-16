ec59849e956680e866f2260b8252ed89138336fc
unknown <vincent.pradipta@binus.ac.id>
2024-05-16 23:39:59 +0700

    Update: Update the populate db file

--- populate_random_challenges.js
+++ populate_random_challenges.js
@@ -1,5 +1,9 @@
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
 function getRandomTitle(length) {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxy';
     let result = '';
     for (let i = 0; i < length; i++) {
         const randomIndex = Math.floor(Math.random() * characters.length);
@@ -12,9 +16,10 @@ function getRandomPoints(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-const main = async () => {
+async function main() {
+    var sum = []
     for (var i = 0; i < 113; i++) {
-        var title = getRandomTitle(10);
+        var title = getRandomTitle(12);
         points = getRandomPoints(10, 25);
         data = {
             challenge_title: title,
@@ -23,9 +28,11 @@ const main = async () => {
             total_test_case: 17
         }
 
-        // await prisma.challenge.create(data);
-        console.log(data)
+        sum.push(data)
     }
+    await prisma.challenge.createMany({
+        data: sum
+    });
 }
 
 main()
