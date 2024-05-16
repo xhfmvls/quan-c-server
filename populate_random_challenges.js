9f8fc6df8b3f67f03a3fb79eb958dd7850b6b1b5
unknown <vincent.pradipta@binus.ac.id>
2024-05-16 23:16:38 +0700

    ADD: Script for challenges list for pagination testing purposes

---
+++ populate_random_challenges.js
@@ -0,0 +1,26 @@
+function getRandomTitle(length) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        result += characters[randomIndex];
+    }
+    return result;
+}
+
+function getRandomPoints(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+for (var i = 0; i < 113; i++) {
+    var title = getRandomTitle(10);
+    points = getRandomPoints(10, 25);
+    data = {
+        challenge_title: title,
+        repo_link: `https://github.com/xhfmvls/${title}`,
+        points: points,
+        total_test_case: 17
+    }
+
+    await prisma.challenge.create(data);
+}
