e8dc02bc28b3bdce06ad8667a3201b68c4994d6d
unknown <vincent.pradipta@binus.ac.id>
2024-08-26 02:07:04 +0700

    ADD: Add tests for submitChallenge controller; Add import creds for better security

--- tests/challenges.test.js
+++ tests/challenges.test.js
@@ -1,77 +1,96 @@
 const request = require("supertest");
 const { app, server } = require('../dist/index');
 const path = require('path');
-const sampleUserId = 'a9b4582d-5f95-4187-b399-15af0ea1f482';
-const authToken = 'Bearer gho_';
+const { sampleUserId, authToken, authTokenUser } = require('./constants');
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
 
 describe("Challenges Controller", () => {
 
-    //   describe("submitChallege", () => {
-    //     it("should return 200 and success message when challenge is submitted successfully", async () => {
-    //       const response = await request(app)
-    //         .post("/submitChallenge") // Updated to match your route
-    //         .set("Authorization", "Bearer yourToken") // Include any necessary headers
-    //         .attach("file", path.join(__dirname, "sample.zip")) // Adjust to the correct path for your test file
-    //         .field("title", "Sample Challenge")
-    //         .field("link", "http://example.com")
-    //         .field("points", 10)
-    //         .field("total_test_case", 5)
-    //         .field("tags", ["tag1", "tag2"]);
+    describe("submitChallege", () => {
+        it("should return 200 and success message when challenge is submitted successfully", async () => {
+            const response = await request(app)
+                .post("/submitChallenge")
+                .set("Authorization", authToken)
+                .attach("file", path.join(__dirname, "test_files", "LFI.zip"))
+                .field("title", "Test_Sample")
+                .field("link", "https://github.com/xhfmvls/PathGuard")
+                .field("points", 10)
+                .field("total_test_case", 10)
+                .field("tags", ["tag1", "tag2"]);
 
-    //       expect(response.status).toBe(200);
-    //       expect(response.body.success).toBe(true);
-    //       expect(response.body.message).toContain("Challenge submitted successfully");
-    //     });
+            console.log(response.body);
+            expect(response.status).toBe(200);
+            expect(response.body.success).toBe(true);
+            expect(response.body.message).toContain("Challenge submitted successfully");
+        });
 
-    //     it("should return 400 if any required fields are missing", async () => {
-    //       const response = await request(app)
-    //         .post("/submitChallenge")
-    //         .set("Authorization", "Bearer yourToken")
-    //         .attach("file", path.join(__dirname, "sample.zip"))
-    //         .field("title", "Sample Challenge")
-    //         .field("points", 10)
-    //         .field("total_test_case", 5)
-    //         .field("tags", ["tag1", "tag2"]);
+        it("should return error if any required fields are missing", async () => {
+            const response = await request(app)
+                .post("/submitChallenge")
+                .set("Authorization", authToken)
+                .attach("file", path.join(__dirname, "test_files", "LFI.zip"))
+                .field("title", "Sample Challenge")
+                .field("points", 10)
+                .field("total_test_case", 5)
+                .field("tags", ["tag1", "tag2"]);
 
-    //       expect(response.status).toBe(400);
-    //       expect(response.body).toHaveProperty("error");
-    //       expect(response.body.error).toEqual("All fields are required");
-    //     });
+            expect(response.status).toBe(500);
+            expect(response.body.success).toBe(false);
+            expect(response.body.message).toEqual("All fields are required");
+        });
 
-    //     it("should return 400 if file is not a zip", async () => {
-    //       const response = await request(app)
-    //         .post("/submitChallenge")
-    //         .set("Authorization", "Bearer yourToken")
-    //         .attach("file", path.join(__dirname, "sample.txt")) // Test with a non-zip file
-    //         .field("title", "Sample Challenge")
-    //         .field("link", "http://example.com")
-    //         .field("points", 10)
-    //         .field("total_test_case", 5)
-    //         .field("tags", ["tag1", "tag2"]);
+        it("should return error if file is not a zip", async () => {
+            const response = await request(app)
+                .post("/submitChallenge")
+                .set("Authorization", authToken)
+                .attach("file", path.join(__dirname, "test_files", "sample.txt"))
+                .field("title", "Sample Challenge 2")
+                .field("link", "https://github.com/xhfmvls/PathGuard")
+                .field("points", 10)
+                .field("total_test_case", 10)
+                .field("tags", ["tag1", "tag2"]);
 
-    //       expect(response.status).toBe(400);
-    //       expect(response.body).toHaveProperty("error");
-    //       expect(response.body.error).toEqual("Only zip files are allowed");
-    //     });
+            expect(response.status).toBe(500);
+            expect(response.body.success).toBe(false);
+            expect(response.body.message).toEqual("Only zip files are allowed");
+        });
 
-    //     it("should return 400 if no file is uploaded", async () => {
-    //       const response = await request(app)
-    //         .post("/submitChallenge")
-    //         .set("Authorization", "Bearer yourToken")
-    //         .field("title", "Sample Challenge")
-    //         .field("link", "http://example.com")
-    //         .field("points", 10)
-    //         .field("total_test_case", 5)
-    //         .field("tags", ["tag1", "tag2"]);
+        it("should return error if no file is uploaded", async () => {
+            const response = await request(app)
+                .post("/submitChallenge")
+                .set("Authorization", authToken)
+                .field("title", "Sample Challenge 3")
+                .field("link", "https://github.com/xhfmvls/PathGuard")
+                .field("points", 10)
+                .field("total_test_case", 10)
+                .field("tags", ["tag1", "tag2"]);
 
-    //       expect(response.status).toBe(400);
-    //       expect(response.body).toHaveProperty("error");
-    //       expect(response.body.error).toEqual("No file uploaded");
-    //     });
-    //   });
+            expect(response.status).toBe(500);
+            expect(response.body.success).toBe(false);
+            expect(response.body.message).toEqual("No file uploaded");
+        });
+
+        it("should return error if the token is not admin token", async () => {
+            const response = await request(app)
+                .post("/submitChallenge")
+                .set("Authorization", authTokenUser)
+                .attach("file", path.join(__dirname, "test_files", "LFI.zip"))
+                .field("title", "Sample Challenge 4")
+                .field("link", "https://github.com/xhfmvls/PathGuard")
+                .field("points", 10)
+                .field("total_test_case", 10)
+                .field("tags", ["tag1", "tag2"]);
+
+            expect(response.status).toBe(500);
+            expect(response.body.success).toBe(false);
+            expect(response.body.message).toEqual("Unauthorized");
+        });
+    });
 
     describe("getChallenges", () => {
-        it("should return 200 and the incomplete challenges'", async () => {
+        it("should return 200 and the incomplete challenges", async () => {
             const response = await request(app)
                 .post("/getChallenges") // Updated to match your route
                 .set("Authorization", authToken)
@@ -83,7 +102,24 @@ describe("Challenges Controller", () => {
                     difficulty: "all",
                 });
 
-            console.log(response.body);
+            expect(response.status).toBe(200);
+            expect(response.body.success).toBe(true);
+            expect(response.body).toHaveProperty("data");
+            expect(response.body.message).toEqual("Challenges fetched successfully");
+        });
+
+        it("should return 200 and the newly created challenge (submitChallege test)", async () => {
+            const response = await request(app)
+                .post("/getChallenges") // Updated to match your route
+                .set("Authorization", authToken)
+                .send({
+                    userId: sampleUserId,
+                    filter: "incomplete",
+                    page: 1,
+                    search: "Test_Sample",
+                    difficulty: "all",
+                });
+
             expect(response.status).toBe(200);
             expect(response.body.success).toBe(true);
             expect(response.body).toHaveProperty("data");
@@ -102,7 +138,6 @@ describe("Challenges Controller", () => {
                     difficulty: "easy",
                 });
 
-            console.log(response.body);
             expect(response.status).toBe(200);
             expect(response.body.success).toBe(true);
             expect(response.body).toHaveProperty("data");
@@ -121,7 +156,6 @@ describe("Challenges Controller", () => {
                     difficulty: "easy",
                 });
 
-            console.log(response.body);
             expect(response.status).toBe(200);
             expect(response.body.success).toBe(true);
             expect(response.body.data).toEqual([]);
@@ -129,7 +163,7 @@ describe("Challenges Controller", () => {
         });
 
         it("should return error if the token is invalid", async () => {
-            const invalidToken = "Bearer gho_aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            const invalidToken = "Bearer gho_fakedataaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
 
             const response = await request(app)
                 .post("/getChallenges")
@@ -142,13 +176,49 @@ describe("Challenges Controller", () => {
                     difficulty: "all",
                 });
 
-            console.log(response.body);
             expect(response.status).toBe(500);
             expect(response.body.success).toBe(false);
         });
     });
 });
 
+const deleteTagAssigns = async (challengeId) => {
+    const tagAssigns = await prisma.tagAssign.findMany({
+        where: {
+            challenge_id: challengeId
+        }
+    });
+
+    for (const tagAssign of tagAssigns) {
+        await prisma.tagAssign.delete({
+            where: {
+                tag_assign_id: tagAssign.tag_assign_id
+            }
+        });
+    }
+}
+
+const deleteChallengesData = async () => {
+    const challenge = await prisma.challenge.findFirst({
+        where: {
+            challenge_title: "Test_Sample"
+        }
+    });
+
+    if (challenge) {
+        const challengeId = challenge.challenge_id;
+
+        await deleteTagAssigns(challengeId);
+
+        await prisma.challenge.delete({
+            where: {
+                challenge_id: challengeId
+            }
+        });
+    }
+}
+
 afterAll((done) => {
     server.close(done);
+    deleteChallengesData();
 });
